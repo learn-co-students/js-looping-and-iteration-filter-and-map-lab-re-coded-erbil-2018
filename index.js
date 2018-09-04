15b2875462699a6feb33efd5aafcb30d4b6bff32
Ahmad Khatab Ahmad <ahmadzendane725@gmail.com>
2018-09-04 08:01:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution here:
+function driversWithRevenueOver(driversArray,revenue)
+{
+  let copyArray =[];
+  
+  for(var driver of driversArray)
+  {
+    if(driver.revenue>revenue)
+     copyArray.push(driver)
+  }
+  return copyArray;
+}
+function driverNamesWithRevenueOver(driversArray,revenue)
+{
+  let copyArray=[];
+  
+  for(var driver of driversArray)
+   {
+     if(driver.revenue>revenue)
+      copyArray.push(driver.name);
+   }
+   return  copyArray;
+}
+function exactMatch (drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
+
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
+    }
+
+    return matches;
+  });
+}
+
+function exactMatchToList(arr,obj)
+{
+  return arr.filter(index => {
+    let bool = false
+    for(const key in obj)
+     bool = index[key] === obj[key];
+     return bool
+  }).map(index => index.name)
+}
